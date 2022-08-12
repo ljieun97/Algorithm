3b2f876773969a474f68ac988ddd15bd16039d85
ljieun <wldms8196@gmail.com>
2022-08-12 20:49:42 +0900

    [java] 내적

---
+++ programmers/level1/src/naejuk/Main.java
@@ -0,0 +1,20 @@
+package naejuk;
+
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i=0; i<a.length; i++) {
+            answer += a[i]*b[i];
+        }
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] a = {1,2,3,4};
+        int[] b = {-3,-1,0,2};
+        System.out.println(s.solution(a, b));
+    }
+}
