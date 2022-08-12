4d3c1dc7e3d8fe2553f1280ecb60df6d8224734c
ljieun <wldms8196@gmail.com>
2022-08-12 22:15:00 +0900

    [java] 소수 만들기

---
+++ programmers/level1/src/sosu/Main.java
@@ -0,0 +1,39 @@
+package sosu;
+
+class Solution {
+    public static boolean sosu (int sum) {
+        boolean answer = false;
+        for (int i=2; i<sum; i++) {
+            if (sum%i==0) {
+                answer = false;
+                break;
+            } else {
+                answer = true;
+            }
+        }
+        return answer;
+    }
+
+    public int solution(int[] nums) {
+        int answer = 0;
+        int num = 0;
+        for (int i=0; i<nums.length; i++) {
+            for (int j=i+1; j<nums.length; j++) {
+                for (int k=j+1; k<nums.length; k++) {
+                    num=nums[i]+nums[j]+nums[k];
+                    if(num>2 && sosu(num)) answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 2, 7, 6, 4};
+        System.out.println(s.solution(nums));
+    }
+}
+
