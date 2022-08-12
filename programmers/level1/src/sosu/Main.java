6ab3fd90243f31898f0ab8c77edb0c2d62813ca1
ljieun <wldms8196@gmail.com>
2022-08-12 22:17:28 +0900

    [java] 소수 만들기

--- programmers/level1/src/sosu/Main.java
+++ programmers/level1/src/sosu/Main.java
@@ -16,12 +16,11 @@ class Solution {
 
     public int solution(int[] nums) {
         int answer = 0;
-        int num = 0;
-        for (int i=0; i<nums.length; i++) {
-            for (int j=i+1; j<nums.length; j++) {
+        for (int i=0; i<nums.length-2; i++) {
+            for (int j=i+1; j<nums.length-1; j++) {
                 for (int k=j+1; k<nums.length; k++) {
-                    num=nums[i]+nums[j]+nums[k];
-                    if(num>2 && sosu(num)) answer++;
+                    int num=nums[i]+nums[j]+nums[k];
+                    if(sosu(num)) answer++;
                 }
             }
         }
