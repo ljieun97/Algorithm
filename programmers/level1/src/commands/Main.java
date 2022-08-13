56f43fbe3c00d18bf085c219f1fe62a5241aecb3
ljieun <wldms8196@gmail.com>
2022-08-13 17:23:50 +0900

    [java] copyOfRange

---
+++ programmers/level1/src/commands/Main.java
@@ -0,0 +1,23 @@
+package commands;
+
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int i=0; i<commands.length; i++) {
+            int[] arr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        System.out.println(Arrays.toString(s.solution(array, commands)));
+    }
+}
